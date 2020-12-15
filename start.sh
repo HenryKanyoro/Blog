ecc5d4b9f9f52b1a8b08c6e49e690ed18b7720c0
HenryKanyoro <kanyorohenry6@gmail.com>
2020-12-15 23:07:12 +0300

    final commit

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export SECRET_KEY='kaihavertz'
-export MAIL_USERNAME='kerrykomar@gmail.com'
-export MAIL_PASSWORD='dellakomar'
+export SECRET_KEY='henry'
+export MAIL_USERNAME='henrykanyoro@gmail.com'
+export MAIL_PASSWORD='sokomoko254'
 
 python3.8 manage.py server
